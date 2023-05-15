528b91aabf225d5f3205cd72b7e5fa109f4becb1
drowe67 <david@rowetel.com>
2023-05-15 17:29:30 +0930

    blanker.m simulation showing R<B and R>B case, added impulse noise section for R<<B, baseband pulse train

--- emi/blanker.m
+++ emi/blanker.m
@@ -1,31 +1,71 @@
 % blanker.m
 % Plot spectrum for noise blanker output
 
-1;
+function blanker(epslatex=0)
+    if epslatex
+        textfontsize = get(0,"defaulttextfontsize");
+        linewidth = get(0,"defaultlinelinewidth");
+        set(0, "defaulttextfontsize", 10);
+        set(0, "defaultaxesfontsize", 10);
+        set(0, "defaultlinelinewidth", 0.5);
+    end
 
-function plot_blanker(f_sig_hz, amp, f_max_hz, pulse_rate_hz, pulse_duty_cycle, c, leg='')
-  centre_amp = (1-pulse_duty_cycle)*amp;
-  N = round(f_max_hz/pulse_rate_hz);
-  sideband_amp = amp*pulse_duty_cycle*sinc(pi*(1:N)*pulse_duty_cycle);
-  amps = 20*log10([fliplr(sideband_amp) centre_amp sideband_amp]);
-  freqs = f_sig_hz + [-fliplr(pulse_rate_hz*(1:N)) 0 pulse_rate_hz*(1:N)];
-  stem(freqs,amps,c,'basevalue',-100)
-  if length(leg)
-    legend(leg);
-  end
+    bandwidth_hz = 20E3;  f_max_hz = bandwidth_hz/2;
+    figure(1); clf; subplot(211); hold on;
+    pulse_period = 20E-3; pulse_length = 100E-6;
+    plot_blanker(0, 1, f_max_hz, 1/pulse_period, pulse_length, 'b')
+    grid; ylabel('dB')
+    axis([-f_max_hz f_max_hz -100 0]);
+    hold off;
+
+    subplot(212);
+    hold on;
+    pulse_period = 1/5000; pulse_length = 100E-6;
+    plot_blanker(0, 1, f_max_hz, 1/pulse_period, pulse_length, 'b')
+    grid; ylabel('dB')
+    axis([-f_max_hz f_max_hz -100 0]);
+    hold off;
+
+    if epslatex
+        fn = "blanker.tex";
+        print(fn,"-depslatex","-S300,300");
+        printf("printing... %s\n", fn);
+        set(0, "defaulttextfontsize", textfontsize);
+        set(0, "defaultaxesfontsize", textfontsize);
+        set(0, "defaultlinelinewidth", linewidth);
+    end
+
+    bandwidth_hz = 1E6;  f_max_hz = bandwidth_hz/2;
+    figure(2); clf;
+    hold on;
+    pulse_period = 1/100E3; pulse_length = 5E-6;
+    plot_blanker(0, 1, f_max_hz, 1/pulse_period, pulse_length, 'b')
+    grid; ylabel('dB')
+    axis([-f_max_hz f_max_hz -100 0]);
+    hold off;
+   
+
 endfunction
 
-bandwidth_hz = 20E3;  f_max_hz = bandwidth_hz/2;
-figure(1); clf; hold on;
-pulse_period = 20E-3; pulse_length = 100E-6;
-plot_blanker(1000, 1, f_max_hz, 1/pulse_period, pulse_length/pulse_period, 'b')
-grid;
-axis([-f_max_hz f_max_hz -100 0]);
-hold off;
+function plot_blanker(f_sig_hz, amp, f_max_hz, pulse_rate_hz, pulse_length_s, c='b')
+    pulse_duty_cycle = pulse_length_s*pulse_rate_hz;
+    centre_amp = (1-pulse_duty_cycle)*amp;
+    N = round(f_max_hz/pulse_rate_hz);
+    sideband_amp = amp*pulse_duty_cycle*sinc(pi*(1:N)*pulse_duty_cycle);
+    amps = 20*log10([fliplr(sideband_amp) centre_amp sideband_amp]);
+    freqs = f_sig_hz + [-fliplr(pulse_rate_hz*(1:N)) 0 pulse_rate_hz*(1:N)];
+    stem(freqs,amps,c,'basevalue',-100,'markersize',3');
 
-figure(2); clf; hold on;
-pulse_period = 1/5000; pulse_length = 100E-6;
-plot_blanker(1000, 1, f_max_hz, 1/pulse_period, pulse_length/pulse_period, 'b')
-grid;
-axis([-f_max_hz f_max_hz -100 0]);
-hold off;
+    if pulse_rate_hz > 1000
+      leg = sprintf("R=%dkHz T=%3.0fuS",pulse_rate_hz/1000, pulse_length_s/1E-6);
+      legend('location','east')
+    else
+      leg = sprintf("R=%dHz T=%3.0fuS",pulse_rate_hz, pulse_length_s/1E-6);
+      legend('location','northeast')
+    end
+    legend(leg); legend('boxoff'); legend('left');
+
+    plot([-3000 -3000],[0 -100],'g'); plot([3000 3000],[0 -100],'g')
+
+endfunction
+

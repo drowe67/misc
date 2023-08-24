c7ebb1a57415ccb0e175b6f20fb71a2eaa5b8e97
drowe67 <david@rowetel.com>
2023-08-24 11:07:33 +0930

    test_resampler - instructive look time domain effect of resampling Doppler

--- freedv_low/equaliser.m
+++ freedv_low/equaliser.m
@@ -9,13 +9,43 @@ pkg load signal;
 
 % simple time domain example of resampling
 function test_resampler
-  Fs = 2;
- 
+    randn('seed',1);
+
+    % Fd is Doppler bandwidth (Hz).  Doppler signal is complex so
+    % this is a total bandwidth of Fd, sperad Fd/2 either side of 0 Hz
+    Fd = 2;          
+    Fs = 8000; T=1/Fs; Trun=5;
+    N  = Fs*Trun;
+    [d1 states] = doppler_spread(Fd, Fs, N);
+
+    % Decimate down to pilot symbol rate of 1/(Ts*Np)sample rate 6.25Hz (Np=8 for 700D)
+    Ts=0.02; Np=8; Fp=1/(Ts*Np); Tp=1/Fp;
+    M = Fs/Fp;
+    assert (M == floor(M));
+    n2 = 1:M:length(d1);
+    d2 = d1(n2);
+    
+    % attempt resampling at mid point (doubling sample rate of d2), using a 6 point resampler
+    % uses 4 past pilots and 2 future
+    B=pi; n=(-3.5:1.5); h=(B/(2*pi))*sinc(n*B/(2*pi)); h=h./sum(h);
+    d3 = filter(h,1,d2);
+
+    figure(1); clf; 
+    subplot(211); hold on;
+    plot(T*(0:N-1),real(d1));
+    stem(Tp*(0:length(d2)-1),real(d2));
+    stem(Tp*(0:length(d2)-1) - 3.5*Tp,real(d3));
+    hold off; axis([0 Trun -1 1])
+    subplot(212); hold on;
+    plot(T*(0:N-1),imag(d1));
+    stem(Tp*(0:length(d2)-1),imag(d2));
+    stem(Tp*(0:length(d2)-1) - 3.5*Tp,imag(d3));
+    hold off; axis([0 Trun -1 1])
 end
 
 % plot frequency response of different resamplers
 function plot_resampler_freq_response
-    figure(1); clf; hold on;
+    figure(2); clf; hold on;
     h = [1 1 1 1]/4; [H w] = freqz(h); plot(w,20*log10(H),'b;[1 1 1 1];');
     B=pi/2; n=(-1.5:1.5); h=(B/(2*pi))*sinc(n*B/(2*pi)); h=h./sum(h); [H w] = freqz(h); plot(w,20*log10(H),'g;sinc pi/2;');
     B=pi; n=(-1.5:1.5); h=(B/(2*pi))*sinc(n*B/(2*pi)); h=h./sum(h); [H w] = freqz(h); plot(w,20*log10(H),'r;sinc pi;');
@@ -33,7 +63,8 @@ function test_resampler_ber
 % equalise using channel estimate
 % demodulate and count BER
 % plot curves from different resamplers
-% next step: effect of samples from carriers either side, need to simulate that
+% next step: effect of samples from carriers either side, so pilots with slightly different channel
+% next step, extend to sum of two Doppler sequences, after ISI has settled.  Sum of two might improve (central limit)
 end
 
 % Resampling Doppler signal

52d2176a69664b656fe2d720b49d8ba11eed7f6b
drowe67 <david@rowetel.com>
2023-10-22 09:20:39 +1030

    print_eps_restore() helper function

--- freedv_low/ofdm_lib.m
+++ freedv_low/ofdm_lib.m
@@ -109,17 +109,23 @@ function [spread1 spread2 hf_gain path_delay_samples] = gen_hf(ch,Fs,nsam)
 endfunction
 
 function [textfontsize linewidth] = set_fonts(font_size=12)
-    textfontsize = get(0,"defaulttextfontsize");
-    linewidth = get(0,"defaultlinelinewidth");
-    set(0, "defaulttextfontsize", font_size);
-    set(0, "defaultaxesfontsize", font_size);
-    set(0, "defaultlinelinewidth", 0.5);
+  textfontsize = get(0,"defaulttextfontsize");
+  linewidth = get(0,"defaultlinelinewidth");
+  set(0, "defaulttextfontsize", font_size);
+  set(0, "defaultaxesfontsize", font_size);
+  set(0, "defaultlinelinewidth", 0.5);
 end
 
 function restore_fonts(textfontsize,linewidth)
-    set(0, "defaulttextfontsize", textfontsize);
-    set(0, "defaultaxesfontsize", textfontsize);
-    set(0, "defaultlinelinewidth", linewidth);
+  set(0, "defaulttextfontsize", textfontsize);
+  set(0, "defaultaxesfontsize", textfontsize);
+  set(0, "defaultlinelinewidth", linewidth);
+end
+
+function print_eps_restore(fn,sz,textfontsize,linewidth)
+  print(fn,"-depslatex",sz);
+  printf("printing... %s\n", fn);
+  restore_fonts(textfontsize,linewidth);
 end
 
 function draw_ellipse(x1,y1,a,b,angle, leg)

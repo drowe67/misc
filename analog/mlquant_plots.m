1c5ad8899b878bd0bebb3023a33c0b3ca03aa8f5
drowe67 <david@rowetel.com>
2024-01-20 09:41:16 +1030

    extending to VQs for other systems

--- analog/mlquant_plots.m
+++ analog/mlquant_plots.m
@@ -2,7 +2,7 @@
 
 1;
 
-function uniform_vq(epslatex=0)
+function uniform_vq(prefix="vq_train", epslatex=0)
   util;
 
   if epslatex, [textfontsize linewidth] = set_fonts(font_size=14); end
@@ -10,12 +10,12 @@ function uniform_vq(epslatex=0)
   uniform_bits = [10 20 30 40 50];
   uniform_var  = [0.0833 0.0208 0.0052 0.0013 0.0003];
   
-  load vq_train_var1.txt
-  load vq_train_var2.txt
-  load vq_train_var3.txt
+  var1 = load(sprintf("%s_var1.txt",prefix));
+  var2 = load(sprintf("%s_var2.txt",prefix));
+  var3 = load(sprintf("%s_var3.txt",prefix));
   
-  vq_bits = [log2(vq_train_var1(:,1)); 12+log2(vq_train_var1(:,1));  24+log2(vq_train_var1(:,1))];
-  vq_var  = [vq_train_var1(:,2); vq_train_var2(:,2); vq_train_var3(:,2)];
+  vq_bits = [log2(var1(:,1)); 12+log2(var1(:,1));  24+log2(var1(:,1))];
+  vq_var  = [var1(:,2); var2(:,2); var3(:,2)];
 
   semilogy(uniform_bits, uniform_var,'b+-;Uniform;');
   hold on; semilogy(vq_bits, vq_var,'g+-;VQ;'); hold off;
